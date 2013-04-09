212f79bdadcf008fbc32d8defd0eae7589c93fe3
Justin Workman <xtagon@gmail.com>
2013-04-08 22:18:48 -0700

    Add helper methods for one-off API calls.

--- lib/pandorified/result.rb
+++ lib/pandorified/result.rb
@@ -9,36 +9,18 @@ module Pandorified
 			@xml = Nokogiri::XML(RestClient.post(API_URL, params))
 		end
 
-		# Check the status of this result. See {#success?} and {#error?}.
-		#
-		# @return [Number] A status number as returned by Pandorabots.
-		def status
-			@status ||= @xml.xpath('/result/@status').first.value.to_i
-		end
-
-		# @return [String] The botid of the bot this result is for.
-		def botid
-			@botid ||= @xml.xpath('/result/@botid').first.value
-		end
-
-		# @return [String] The custid for this session.
-		def custid
-			@custid ||= @xml.xpath('/result/@custid').first.value
-		end
-
-		# @return [String] The orginal input that triggered this response.
-		def input
-			@input ||= @xml.xpath('/result/input').first.text
-		end
-
 		# @return [String] The bot's response to the input.
 		def that
 			@that ||= @xml.xpath('/result/that').first.text
 		end
 
-		# @return [String] The error message as returned by Pandorabots, if an error occured.
-		def message
-			@message ||= @xml.xpath('/result/message').first.text
+		alias_method :to_s, :that
+
+		# Check the status of this result. See {#success?} and {#error?}.
+		#
+		# @return [Number] A status number as returned by Pandorabots.
+		def status
+			@status ||= @xml.xpath('/result/@status').first.value.to_i
 		end
 
 		# @return `true` if this result was successful (no error was returned by Pandorabots), `false` otherwise.
@@ -56,6 +38,28 @@ module Pandorified
 			!self.success?
 		end
 
-		alias_method :to_s, :that
+		# @return [String] The error message as returned by Pandorabots, if an error occured.
+		def message
+			return nil if self.success?
+			@message ||= @xml.xpath('/result/message').first.text
+		end
+
+		alias_method :error, :message
+		alias_method :error_message, :message
+
+		# @return [String] The botid of the bot this result is for.
+		def botid
+			@botid ||= @xml.xpath('/result/@botid').first.value
+		end
+
+		# @return [String] The custid for this session.
+		def custid
+			@custid ||= @xml.xpath('/result/@custid').first.value
+		end
+
+		# @return [String] The orginal input that triggered this response.
+		def input
+			@input ||= @xml.xpath('/result/input').first.text
+		end
 	end
 end
--- lib/pandorified/session.rb
+++ lib/pandorified/session.rb
@@ -7,7 +7,7 @@ module Pandorified
 	class Session
 		# A new session for conversing with a bot.
 		#
-		# @note If you choose not to specify a {custid}, one will be chosen automatically and remembered throughout the session.
+		# @note If you choose not to specify a {custid}, one will be automatically chosen and remembered throughout the session.
 		#
 		# @param [String] botid A valid Pandorabots botid.
 		# @param [String] custid An identifier used to keep track of this conversation.
@@ -18,11 +18,12 @@ module Pandorified
 
 		# Send a message to this session's bot and receive a response.
 		#
-		# @note See {Pandorified::Result} for ways to handle the response, or use {#talk!} which raises an exception on errors.
+		# See {Pandorified::Result} for how to check for an error response and get the error message.
+		# Alternatively, you can use {#talk!} instead of this method, which raises an exception when Pandorabots API returns an error.
 		#
 		# @param [String] input Text to say to the bot.
 		#
-		# @return [String] The bot's response text.
+		# @return [Pandorified::Result] The bot's response as a result object.
 		def talk(input)
 			result = Pandorified::Result.new(botid: @botid, custid: @custid, input: input)
 			@custid ||= result.custid if result.success?
@@ -30,9 +31,9 @@ module Pandorified
 		end
 
 		# Send a message to this session's bot and receive a response (if successful).
-		# If Pandorabots responds with an error, {Pandorified::Exceptions::PandorabotsError} is raised.
 		#
-		# @note See {#talk} if you want to check the {Pandorabots::Result} object for success instead of an exception being raised on error.
+		# If Pandorabots API responds with an error, {Pandorified::PandorabotsError} is raised with the specific error message.
+		# If you'd like to check for and handle the error yourself, you can use {#talk} instead of this method.
 		#
 		# @param [String] input Text to say to the bot.
 		#
@@ -40,7 +41,7 @@ module Pandorified
 		def talk!(input)
 			result = self.talk(input)
 			raise Pandorified::PandorabotsError, "Pandorabots returned status #{result.status}: #{result.message}" if result.error?
-			result
+			result.that
 		end
 	end
 end
